8418c2d5ae603a44e9efd6a010604932003a6f6f
Kyle Cheng <kcheng120@gmail.com>
2025-09-29 16:22:57 -0400

    Implement getComputerChoice and getHumanChoice

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log('Hello, World!');
+let humanScore = 0;
+let computerScore = 0;
+
+function getComputerChoice(){
+    let randomChoice = Math.floor(Math.random() * 3);
+
+    if(randomChoice === 0){
+        return console.log("Rock");
+    }
+    else if(randomChoice === 1){
+        return console.log("Paper");
+    }
+    else{
+        return console.log("Scissors");
+    }
+}
+
+function getHumanChoice(){
+    let choice = prompt("Enter your choice (Rock, Paper, or Scissors):");
+    return choice;
+}
+
+
